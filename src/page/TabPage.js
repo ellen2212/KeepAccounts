758cecfc38e01edcafafb26b07beef8c7325b7e0
Ellen <ellen.wang@ribitwo.com>
2019-03-08 12:21:23 +0800

    import iconfont

--- src/page/TabPage.js
+++ src/page/TabPage.js
@@ -9,7 +9,7 @@ import ChartPage from './ChartPage';
 import RecordPage from './RecordPage';
 import FindPage from './FindPage';
 import MyPage from './MyPage';
-import KStatusBar from '../component/KStatusBar';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const TabNavigator = createBottomTabNavigator({
   Home: HomePage,
@@ -17,6 +17,37 @@ const TabNavigator = createBottomTabNavigator({
   Record : RecordPage,
   Find : FindPage,
   My : MyPage
+},{
+  defaultNavigationOptions: ({navigation})=>({
+    tabBarIcon: ({focused,horizontal,tintColor})=>{
+      const { routeName } = navigation.state;
+      let IconComponent = Ionicons;
+      let iconName;
+      let suffix = '';
+      switch (routeName){
+        case 'Home':
+          iconName = `ios-list-box${suffix}`;
+          break;
+        case 'Chart':
+          iconName = `ios-stats${suffix}`;
+          break;
+        case 'Record':
+          iconName = `ios-add${suffix}`;
+          break;
+        case 'Find':
+          iconName = `ios-compass${suffix}`;
+          break;
+        case 'My':
+          iconName = `ios-contact${suffix}`;
+          break;
+      }
+      return <IconComponent name={iconName} size={25} color={tintColor}/>
+    }
+  }),
+  tabBarOptions: {
+    activeTintColor: '#fed954',
+    inactiveTintColor: 'gray'
+  }
 });
 const Tab = createAppContainer(TabNavigator);
 type Props = {};
